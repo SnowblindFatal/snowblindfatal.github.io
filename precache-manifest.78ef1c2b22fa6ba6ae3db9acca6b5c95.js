934da9d10dd6638186ad47a9aab9c012fb74ad2f
Juho Saarela <juho.saarela@vincit.fi>
2019-10-25 13:25:50 +0300

    asdf

---
+++ precache-manifest.78ef1c2b22fa6ba6ae3db9acca6b5c95.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94b2ce2b7e32749ba298aeeb431fef19",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fce39849a73ff24fc8d3",
+    "url": "/static/css/main.0bd410a5.chunk.css"
+  },
+  {
+    "revision": "9d2741fd94424844c100",
+    "url": "/static/js/2.ed7e0e12.chunk.js"
+  },
+  {
+    "revision": "fce39849a73ff24fc8d3",
+    "url": "/static/js/main.6009c25e.chunk.js"
+  },
+  {
+    "revision": "2581f02186443f529e17",
+    "url": "/static/js/runtime-main.89380d90.js"
+  },
+  {
+    "revision": "281f9ef80406b2bea0c19f01a9cb329b",
+    "url": "/static/media/man.281f9ef8.png"
+  },
+  {
+    "revision": "ecb0f8d66c5e34943fb49382478227b2",
+    "url": "/static/media/toilet.ecb0f8d6.png"
+  }
+]);
